88996b6cea7a44ee23ba6492e128e0b2c6f7c1d4
Cold-TM <124048167+Cold-TM@users.noreply.github.com>
2023-01-31 14:25:59 +0300

    light file shader

---
+++ light.py
@@ -0,0 +1,9 @@
+import glm
+
+class Light:
+	def __init__(self, position=(3, 3, -3), color=(1, 1, 1)):
+		self.position = glm.vec3(position)
+		self.color = glm.vec3(color)
+		self.Ia = 0.1 * self.color
+		self.Id = 0.8 * self.color
+		self.Is = 1.0 * self.color
